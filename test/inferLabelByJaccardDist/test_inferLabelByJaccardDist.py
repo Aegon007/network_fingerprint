ad07bb3bd0b820a466e7aeccf19c1b57fafbc460
carl <gotorutgers@outlook.com>
2018-11-07 14:14:00 -0500

    change name

---
+++ test/inferLabelByJaccardDist/test_inferLabelByJaccardDist.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+import sys
+import unittest
+
+
+class Test_InferLabelByJaccardDist(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_computeJaccardDist(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
